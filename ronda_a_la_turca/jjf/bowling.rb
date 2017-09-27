38895621f25e66b7a9a9cee3f3574a8aed9680a8
Jose Lezama <joseglego@gmail.com>
2017-09-27 15:38:22 -0300

    First versio of score

---
+++ ronda_a_la_turca/jjf/bowling.rb
@@ -0,0 +1,27 @@
+class Bowling
+  attr_accessor :shots, :rush, :frame
+  
+  def record (_shots)
+    if (_shots.length == 1 || _shots.length == 2)
+      if (shots[0] + shots[1] <= 10)
+        shots = shots.push(_shots)
+        frame = frame + (_shots.sum * (1 + rush))
+        rush = rush - 1
+        if (shots[0] == 10)
+          rush = 2
+        elsif (shots[0] + shots[1] == 10)
+          rush = 1
+        end
+      end
+    end
+  end
+
+  def finished
+
+  end
+
+  def score
+  end
+end
+
+bowling = Bowling.new
